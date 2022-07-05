8d0c436c3b09e5c379a0f97f4ad59d7bec62b8c1
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-05 17:35:47 +0200

    create user class and role enum

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,77 @@
+package entities;
+
+import java.text.DateFormat;
+import java.util.Locale;
+import utils.Role;
+
+public class User {
+
+  private String name;
+  private String surname;
+  private String birthDate;
+  private final String username;
+  private String password;
+  private final String id;
+  private final String creationDate;
+  private final Role role;
+
+  public User(String name, String surname, String birthDate, String username, String password,
+      String id, Role role) {
+    this.name = name;
+    this.surname = surname;
+    this.birthDate = birthDate;
+    this.username = username;
+    this.password = password;
+    this.id = id;
+    this.creationDate = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Locale.ITALY).toString();
+    this.role = role;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public String getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(String birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getCreationDate() {
+    return creationDate;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+}
