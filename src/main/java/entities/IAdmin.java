02b67fc93f98ac02d10b637de820ddf411a44218
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-05 18:22:47 +0200

    termineted IAdmin

--- src/main/java/entities/IAdmin.java
+++ src/main/java/entities/IAdmin.java
@@ -1,13 +1,17 @@
 package entities;
 
 import java.util.Optional;
+import utils.Role;
 
 public interface IAdmin {
 
   Optional<User> findByUsername(String username);
-  void insertUser(String name, String surname, String birthDate,String username,String password, String role);
-  void updatePassword(String username);
-  void printUser();
+  void insertUser(String name, String surname, String birthDate, String username,
+      String password, Role role) throws Exception;
+
+  void updatePassword(User user,String newPassword);
+
+  void printUser(User user) ;
   void printAllDBUser();
 
 }
