506ae565a74ddcff603f411fa84686640a81b46a
Paolo Marra <paolo.marra1985@gmail.com>
2022-07-05 18:02:09 +0200

    create interface IAdmin

--- src/main/java/entities/IAdmin.java
+++ src/main/java/entities/IAdmin.java
@@ -1,8 +1,10 @@
 package entities;
 
+import java.util.Optional;
+
 public interface IAdmin {
 
-  User findByUsername(String username);
+  Optional<User> findByUsername(String username);
   void insertUser(String name, String surname, String birthDate,String username,String password, String role);
   void updatePassword(String username);
   void printUser();
